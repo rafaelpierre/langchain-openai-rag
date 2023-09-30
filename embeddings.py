630a1decc6782e3034114b324cc455f96b55d76b
Rafael Pierre <rafael.pierre@databricks.com>
2023-09-30 12:43:10 +0200

    init

--- embeddings.py
+++ embeddings.py
@@ -9,7 +9,6 @@ from chromadb.config import Settings
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.schema.document import Document
-from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -17,29 +16,74 @@ import pandas as pd
 
 logging.basicConfig(
     level = "DEBUG",
-    format = '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
+    format = '%(asctime)s|p%(process)s|%(pathname)s:%(lineno)d|%(levelname)s|%(message)s'
 )
 
 def get_pandas_df(
     database: str = "/Users/rafael.pierre/Downloads/database.sqlite",
     query: str = "SELECT * from content",
-    sample_size: int = 10
+    sample_size: int = -1
 ):
     
     with sqlite3.connect(
         database = database
     ) as con:
         df = pd.read_sql_query(query, con)
-        df = df.sample(n = sample_size)
+        if sample_size > 0:
+            df = df.sample(n = sample_size)
         df = df.loc[:, "content"]
         df.to_csv("content.csv")
 
     return df
 
-def save_embeddings(
-        texts: List[str],
-        path: str = "./chroma_db"
-    ) -> Chroma:
+def get_summaries(
+    text: List[str] = [],
+    max_tokens: int = 256,
+    temperature: float = 0.3,
+    top_p: float = 0.9,
+    best_of: int = 10
+):
+    template = """
+        Assume you are a music expert. Please summarize the following album
+        review from Pitchfork. Make sure to keep info such as release date,
+        musical style, which artists are similar, and the review score.
+
+        Please generate the output as JSON dictionary, like in the example below:
+
+        '''json
+            "artist": "michael jackson",
+            "review": "this is a review",
+            "score": "9.0"
+            "similar_to": ["prince", "madonna"]
+        '''
+
+        {review}
+    """
+
+    prompt = PromptTemplate(
+        template = template,
+        input_variables = ["review"]
+    )
+
+    llm = OpenAI(
+        model = "gpt-3.5-turbo-instruct",
+        temperature = temperature,
+        top_p = top_p,
+        best_of = best_of,
+        max_tokens = max_tokens
+    )
+
+    llm_chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+    summary = llm_chain.batch([{"review": item} for item in text])
+
+    return summary
+
+def create_or_search_embeddings(
+    texts: List[str] = [],
+    path: str = "./chroma_db",
+    query: str = "",
+    n_results: int = 20
+) -> str:
 
     chroma_path = Path(path)
     settings = None
@@ -47,48 +91,84 @@ def save_embeddings(
     openai_embeddings = OpenAIEmbeddings()
 
     if chroma_path.exists():
+        logging.info(f"Loading persisted embeddings...")
         client = PersistentClient(
-            path = "./chroma_db",
+            path = path,
             settings = Settings(anonymized_telemetry=False)
         )
-        settings = client.get_settings()
-
-    db = Chroma.from_documents(
-        documents = docs,
-        embedding = openai_embeddings,
-        persist_directory="./chroma_db",
-        client_settings = settings
-    )
-
-    return db
-
-if __name__ == "__main__":
-
-    df = get_pandas_df()
-    db = save_embeddings(texts = list(df.values))
-    query = "Which bands are similar to Slayer?"
-    reviews = str([
-        f"\nReview: {doc.page_content}\n"
-        for doc in db.similarity_search(
-            query = query,
-            k = 5
+        collection = client.get_collection("langchain")
+        query_result = collection.query(
+            query_embeddings = openai_embeddings.embed_query(query),
+            n_results = n_results
         )
+
+        result = query_result["documents"]
+
+    else:
+        db = Chroma.from_documents(
+            documents = docs,
+            embedding = openai_embeddings,
+            persist_directory = path,
+            client_settings = settings
+        )
+
+        result = db.similarity_search(
+            query = query,
+            k = n_results
+        )
+
+        result = [doc.page_content for doc in result]
+
+    return str([
+        f"\nReview: {doc}\n"
+        for doc in result
     ])
+
+def generate_recommendations(
+    query: str = "",
+    reviews: str = ""
+) -> str:
     
     template = """
-    You are an expert in music.
+    You are an expert in alternative music.
 
-    Based on the Pitchfork reviews below, suggest me a music album.
+    Based on the Pitchfork reviews below, suggest me a music album that is similar to the
+    band or artist "{query}".
+
     Pick one of the albums mentioned below, and in your recommendation
     please tell me why are you recommending me this album.
 
     {reviews}
 
+    Include info, like for instance, when this album was released, which musical style is it,
+    and also the review score.
+
     ANSWER:
     """
 
-    prompt = PromptTemplate(input_variables=["reviews"], template=template)
-    llm = OpenAI()
+    prompt = PromptTemplate(
+        input_variables=["reviews", "query"],
+        template=template
+    )
+    llm = OpenAI(temperature=0.7, max_tokens = 512)
     llm_chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
-    response = llm_chain.run(reviews)
-    print(response)
+    
+    response = llm_chain.run(
+        query = query,
+        reviews = reviews
+    )
+
+    return response
+    
+
+if __name__ == "__main__":
+
+    df = get_pandas_df()
+    query = input()
+    docs = create_or_search_embeddings(
+        texts = list(df.values),
+        query = query,
+        n_results = 10
+    )
+
+
---
+++ test_llm.py
@@ -0,0 +1,15 @@
+from embeddings import get_summaries
+from fixtures.reviews import full_text
+
+def test_summary():
+
+    summaries = get_summaries(
+        text = full_text
+    )
+
+    for summary in summaries:
+        print(summary["text"])
+
+    assert False
+    assert summaries
+    assert len(summaries) > 0
